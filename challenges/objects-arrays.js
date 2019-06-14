1acdf214cb5eb9f1ef116078e70bd4d9f48dc585
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-06-14 10:17:02 -0600

    WIP:Finished Object and Array part of objetcs-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -9,30 +9,54 @@
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
 const tyrannosaurus = {
-  
+  name:'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous'
+}
+
+tyrannosaurus.speak = function() {
+  return `The ${this.name} says RAWERSRARARWERSARARARRRR!`
 }
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const stegosaurus = {
+  name:'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+}
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+const velociraptor = {
+  name:'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '9m',
+  period: 'Late Cretaceous'
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(`The tyrannosaurus weighed ${tyrannosaurus.weight}.`);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(`The velociraptor diet type was ${velociraptor.diet}.`);
 
 // How long was a stegosaurus?
-console.log();
+console.log(`The stegosaurus was ${stegosaurus.length} long.`);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(`The tyrannosaurus lived during the ${tyrannosaurus.period} period.`);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.speak());
 
 
 // ==== Arrays ====
@@ -54,21 +78,40 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+
+for(let i = 0; i < graduates.length; i++) {
+  universities[i] = graduates[i].university;
+}
+
+console.log(universities.sort())
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
-
 The resulting contact information should have a space between the first name and the email information like this: 
 Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+
+for(let i = 0; i < graduates.length; i++) {
+  contactInfo.push(`${graduates[i].first_name}: ${graduates[i].email}`)
+}
+
 console.log(contactInfo);
 
 
-/* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
+/* Request 3: Find out how many universities have the string "Uni" included in their name. 
+  Create a new array called uni that contains them all. Log the result. */
 const uni = [];
-console.log(uni);
+
+for(let i = 0; i < graduates.length; i++) {
+  if (graduates[i].university === "Uni") {
+    uni.push(graduates[i])
+  } else {
+    console.log (`None of the University names include the name Uni in them`)
+  }
+}
+
+console.log(JSON.stringify(uni));
 
 
 // ==== ADVANCED Array Methods ====
@@ -89,15 +132,23 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 
 /* Request 1: .forEach()
 
-The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
+The zoo wants to display both the scientific name and the animal name in front of the habitats. 
+ Return an array with only the animal and scientific names in it.  
+ The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
 const animalNames = [];
+
+zooAnimals.forEach(items => {
+  animalNames.push(`Names: ${items.animal_name}, Scientific: ${items.scientific_name}.`);
+});
+
 console.log(animalNames);
 
 /* Request 2: .map()    
 
-The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
+The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  
+Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the result.
 
 */
 
