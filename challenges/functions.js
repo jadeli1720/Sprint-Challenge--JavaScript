822a4102b51b886cd50b2fdc504a17adec4b4239
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-06-14 11:09:11 -0600

    WIP: having trouble with callbacks in functions.js Moving to other files and will comeback to it

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,4 +1,7 @@
 // ==== Callbacks ====  
+function showResult(result) {
+  console.log('result:', result);
+}
 
 /* Step 1: Create a higher-order function that accepts a callback
   * Create a higher-order function named consume that can take 3 parameters.
@@ -8,24 +11,44 @@
 */
 
 
+function consume(x,y,cb){
+  return cb(x, y)
+}
+
+
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+  * Create a function named greeting that accepts a first and last name and returns 
+  * "Hello first-name last-name, nice to meet you!"
 */
+function add(sum1, sum2) {
+  return (sum1 + sum2)
+}
+
+
+function multiply(num1,num2){
+  return (num1 * num2);
+}
+// multiply(4, 2, showResult);
+
+function greeting (first, last, cb) {
+  
+  return consume(`Hello ${first} ${last}, nice to meet you!`)
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // consume(2,2,add); // 4
 // consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: Because it is a method that can use the variables with in the local scope of myFunction.
 
 
 const external = "I'm outside the function";
